f132c77ad98d1cb48d6153a306ec290f7ab8d41f
MargretDaniela <danielamargret6@gmail.com>
2024-10-10 10:41:53 +0300

    test 1

---
+++ question2.py
@@ -0,0 +1,8 @@
+import math
+
+
+# using a funcions create a program that calculates the volume of a cylinder
+raduis=(input ('Enter the raduis of a cyclinder:'))
+height=(input ('Enter the height of a cyclinder:'))
+pie = math.pi
+volume = pie *raduis**2 *height
